f805068cacf493886329c14f929d3501232c5398
Amit Raj <rajamitnine@gmail.com>
2022-10-12 12:10:32 +0530

    Added some important javaScript statements to the app.js file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215);
     const randomCode = "#" + randomNumber.toString(16);
     document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerHTML = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
 }
 
 // event call:
@@ -10,3 +13,4 @@ document.getElementById("btn").addEventListener("click", getColor);
 
 // init call (initial call):
 getColor();
+
